ca51b5b2f4d536c0e9e9cd4aa428655e53b7d9cb
Felipe Franklin <felipelfbezerra54@gmail.com>
2023-01-23 21:00:19 -0300

    Test configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import scss from 'rollup-plugin-scss'
 import packageJson from "./package.json" assert { type: "json" };
+import { terser } from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -21,10 +23,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       scss(),
+      terser(),
     ],
   },
   {
